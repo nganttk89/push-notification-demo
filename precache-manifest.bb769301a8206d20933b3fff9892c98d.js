614b1a497e9b4ba78d9c49736b4af2131ff718a8
nganfs <ngan.tran@closrclub.com>
2019-03-12 09:42:06 +0700

    Up

---
+++ precache-manifest.bb769301a8206d20933b3fff9892c98d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/push-notification-demo/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "382f66249acb412390b7",
+    "url": "/push-notification-demo/static/js/runtime~main.120a03f9.js"
+  },
+  {
+    "revision": "aad2a7e2b9758323ab0c",
+    "url": "/push-notification-demo/static/js/main.2b4341b8.chunk.js"
+  },
+  {
+    "revision": "1d6be92fefe39040d528",
+    "url": "/push-notification-demo/static/js/2.457b0330.chunk.js"
+  },
+  {
+    "revision": "aad2a7e2b9758323ab0c",
+    "url": "/push-notification-demo/static/css/main.77831dec.chunk.css"
+  },
+  {
+    "revision": "b796eeef56fc90f3a6f8bc2dcf0c554d",
+    "url": "/push-notification-demo/index.html"
+  }
+];
