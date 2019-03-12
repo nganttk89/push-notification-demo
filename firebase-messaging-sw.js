0679e855f2ce99fd1ac5e14f6d14126cbd5aa2ae
nganfs <ngan.tran@closrclub.com>
2019-03-12 10:13:44 +0700

    Up

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/5.8.61/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.8.61/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
 firebase.initializeApp({
   messagingSenderId: '693657651637' // troque pelo seu sender id
